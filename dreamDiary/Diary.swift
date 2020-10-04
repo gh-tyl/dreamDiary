c4588f64956f83a18d8f4f1c1008e6c33ef2c52d
Tyler Inari <s1922054@stu.musashino-u.ac.jp>
2020-10-05 03:41:18 +0900

    update calendar

---
+++ dreamDiary/Diary.swift
@@ -0,0 +1,12 @@
+//
+//  Diary.swift
+//  dreamDiary
+//
+//  Created by Tyler Inari on 2020/10/05.
+//
+
+import UIKit
+
+class Diary: NSObject {
+
+}
---
+++ dreamDiary/SimpleAlert.swift
@@ -0,0 +1,12 @@
+//
+//  SimpleAlert.swift
+//  dreamDiary
+//
+//  Created by Tyler Inari on 2020/10/05.
+//
+
+import UIKit
+
+class SimpleAlert: NSObject {
+
+}
