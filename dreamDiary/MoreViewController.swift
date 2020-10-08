5000a5689b7f83c36cae3bc932a223d023c6f654
Tyler Inari <s1922054@stu.musashino-u.ac.jp>
2020-10-08 21:50:15 +0900

    on the way

---
+++ dreamDiary/MoreViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MoreViewController.swift
+//  dreamDiary
+//
+//  Created by Tyler Inari on 2020/10/08.
+//
+
+import UIKit
+
+class MoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
