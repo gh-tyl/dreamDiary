a0fc8deea86d3a7894f3cf329a014dff9755471e
Tyler Inari <s1922054@stu.musashino-u.ac.jp>
2020-10-23 00:15:09 +0900

    ready for new alarm

--- dreamDiary/Alarm/SleepingViewController.swift
+++ dreamDiary/Alarm/SleepingViewController.swift
@@ -2,24 +2,4 @@ import UIKit
 
 class SleepingViewController: UIViewController {
     
-    //インスタンスを生成
-    var currentTime = CurrentTime()
-    
-    @IBOutlet var timeLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func closeBtnWasPressed(_ sender: UIButton) {
-        //前のViewControllerに戻る
-        dismiss(animated: true, completion: nil)
-    }
-    
-    func updateTime(_ time:String) {
-        timeLabel.text = time
-    }
-    
 }
